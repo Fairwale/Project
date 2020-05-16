73016f8809a98b0966e28fbb0733682fbdc82e95
Fairwale <lantsovyaroslav@gmail.com>
2020-05-16 13:53:57 +0300

    Stats Updater class added

---
+++ Shop/StatsUpdater.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public abstract class StatsUpdater
+    {
+        protected string FilePath { get; set; }
+
+        public StatsUpdater(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public virtual void Update(Dictionary<string, int> cart, string name, int money)
+        {
+
+        }
+
+
+    }
+}
