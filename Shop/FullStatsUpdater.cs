f09f4f65bba4c9ddd6b6fee90a748bd5fa8ae4ec
Fairwale <lantsovyaroslav@gmail.com>
2020-05-16 15:40:26 +0300

    Stats Updater to keep track of user purchases

---
+++ Shop/FullStatsUpdater.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using System.Windows;
+
+namespace Shop
+{
+    class Obj2
+    {
+        public List<Sell> Sells { get; set; } = new List<Sell>();
+    }
+
+    class Sell
+    {
+        public string Time { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public Dictionary<string, int> Goods { get; set; } = new Dictionary<string, int>();
+    }
+
+    public class FullStatsUpdater : StatsUpdater
+    {
+        public FullStatsUpdater(string filePath) : base(filePath)
+        {
+
+        }
+
+        public override void Update(Dictionary<string, int> cart, string name, int money)
+        {
+            try
+            {
+                if (!File.Exists(FilePath))
+                {
+                    string time = DateTime.Now.Day + "." + DateTime.Now.Month + "." + DateTime.Now.Year + " " + DateTime.Now.Hour + ":" + DateTime.Now.Minute;
+                    Sell s = new Sell()
+                    {
+                        Time = time,
+                        Name = name,
+                        Cost = money,
+                        Goods = cart
+                    };
+
+                    Obj2 obj = new Obj2();
+                    obj.Sells.Add(s);
+
+                    File.WriteAllText(FilePath, JsonConvert.SerializeObject(obj));
+                }
+                else
+                {
+                    Obj2 obj = JsonConvert.DeserializeObject<Obj2>(File.ReadAllText(FilePath));
+
+                    string time = DateTime.Now.Day + "." + DateTime.Now.Month + "." + DateTime.Now.Year + " " + DateTime.Now.Hour + ":" + DateTime.Now.Minute;
+                    Sell s = new Sell()
+                    {
+                        Time = time,
+                        Name = name,
+                        Cost = money,
+                        Goods = cart
+                    };
+
+                    obj.Sells.Add(s);
+
+                    File.WriteAllText(FilePath, JsonConvert.SerializeObject(obj));
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Ошибка записи о совершённой покупке. Ошибка: " + ex.Message);
+            }
+        }
+
+
+    }
+}
---
+++ Shop/ShortStatsUpdater.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using System.Windows;
+
+namespace Shop
+{
+    public class ShortStatsUpdater : StatsUpdater
+    {
+        public ShortStatsUpdater(string filePath) : base(filePath)
+        {
+
+        }
+
+        public override void Update(Dictionary<string, int> cart, string name, int money)
+        {
+            try
+            {
+                if (!File.Exists(FilePath))
+                {
+                    string time = DateTime.Now.Day + "." + DateTime.Now.Month + "." + DateTime.Now.Year + " " + DateTime.Now.Hour + ":" + DateTime.Now.Minute;
+                    Sell s = new Sell()
+                    {
+                        Time = time,
+                        Name = name,
+                        Cost = money
+                    };
+
+                    Obj2 obj = new Obj2();
+                    obj.Sells.Add(s);
+
+                    File.WriteAllText(FilePath, JsonConvert.SerializeObject(obj));
+                }
+                else
+                {
+                    Obj2 obj = JsonConvert.DeserializeObject<Obj2>(File.ReadAllText(FilePath));
+
+                    string time = DateTime.Now.Day + "." + DateTime.Now.Month + "." + DateTime.Now.Year + " " + DateTime.Now.Hour + ":" + DateTime.Now.Minute;
+                    Sell s = new Sell()
+                    {
+                        Time = time,
+                        Name = name,
+                        Cost = money
+                    };
+
+                    obj.Sells.Add(s);
+
+                    File.WriteAllText(FilePath, JsonConvert.SerializeObject(obj));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка записи о совершённой покупке. Ошибка: " + ex.Message);
+            }
+        }
+
+
+    }
+}
